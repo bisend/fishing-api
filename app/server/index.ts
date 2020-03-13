ba67a2493aec330ec0b2fa559dc2870ffec214ee
bisend <vboychenko54@gmail.com>
2020-03-13 16:58:38 +0200

    feat: added auth routes

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,5 +1,8 @@
-const express = require('express');
+import express from 'express';
+import Router from './routes';
 const app = express();
+
+app.use(Router);
 
 app.listen(3000, 'localhost', async (err) => {
   if (err) {
